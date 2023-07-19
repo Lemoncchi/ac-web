b8d99f55a41201a309b075dad97f8e3e71bcf4f6
cmq <2826395873@qq.com>
2023-07-19 13:36:34 +0800

    优化代码：完善加解密代码、user数据库添加密钥的存储以及初始化

--- acweb/models.py
+++ acweb/models.py
@@ -6,12 +6,16 @@ from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from acweb import db
+import security_code
 
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20))
     password_hash = db.Column(db.String(128))
+    public_key = db.Column(db.LargeBinary(2048))
+    private_key = db.Column(db.LargeBinary(2048))
+    symmetric_key = db.Column(db.LargeBinary(32))
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -19,10 +23,18 @@ class User(db.Model, UserMixin):
     def validate_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    # 为用户生成一个随机公私钥对
+    def public_private_key(self):
+        self.public_key, self.private_key = security_code.encrypt_generate()
+
+    # 为用户生成一个随机对称密钥
+    def symmetric_key(self):
+        self.symmetric_key = security_code.symmetric_generate()
+
 
 class CloudFile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 外键
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))  # 外键
     timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)  # 默认设置为当前时间
     file_name = db.Column(db.String(60))
     file_save_name = db.Column(db.String(60))
@@ -30,63 +42,66 @@ class CloudFile(db.Model):
     file_size = db.Column(db.Integer)
     is_shared = db.Column(db.Boolean, default=False)
 
-
     def __repr__(self):
-        return f'<CloudFile {self.file_name}, {self.file_save_name}, {self.cloud_file.file_hash}, {self.cloud_file.file_size}>'
-
+        return f"<CloudFile {self.file_name}, {self.file_save_name}, {self.cloud_file.file_hash}, {self.cloud_file.file_size}>"
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'timestamp': self.timestamp,
-            'file_name': self.file_name,
-            'file_save_name': self.file_save_name,
-            'file_hash': self.file_hash,
-            'file_size': self.file_size,
-            'is_shared': self.is_shared
+            "id": self.id,
+            "timestamp": self.timestamp,
+            "file_name": self.file_name,
+            "file_save_name": self.file_save_name,
+            "file_hash": self.file_hash,
+            "file_size": self.file_size,
+            "is_shared": self.is_shared,
         }
-    
+
     @staticmethod
-    def save_encrypt_commit(user_id, file_name_, content_bytes_:bytes, is_shared_=False):
-        """保存文件元数据到数据库 & 保存加密后的文件到本地
-        """
+    def save_encrypt_commit(
+        user_id, file_name_, content_bytes_: bytes, is_shared_=False
+    ):
+        """保存文件元数据到数据库 & 保存加密后的文件到本地"""
         file_save_name = file_name_  # TODO: 后面需要对文件名进行处理
-
+        assert len(file_save_name) <= 64, "filename too long (>64B)"  # 文件名长度检测
         file_size_ = len(content_bytes_)
-
-        file_hash_ = None  # TODO: 需要对文件进行安全 hash
+        # assert file_size_ < 1 * 1024 * 1024, "file too large (>=10MB)"  # 文件大小检测
+        file_hash_ = security_code.hash_code(content_bytes_)  # 需要对文件进行安全 hash
 
         encrypted_content_bytes = content_bytes_  # TODO: 需要对文件进行加密
 
-        save_path = os.path.join(app.config['UPLOAD_FOLDER'], file_save_name)
+        save_path = os.path.join(app.config["UPLOAD_FOLDER"], file_save_name)
 
-        cloud_file = CloudFile(user_id=user_id, file_name=file_name_, file_save_name=file_save_name, file_hash=file_hash_, file_size=file_size_, is_shared=is_shared_)
+        cloud_file = CloudFile(
+            user_id=user_id,
+            file_name=file_name_,
+            file_save_name=file_save_name,
+            file_hash=file_hash_,
+            file_size=file_size_,
+            is_shared=is_shared_,
+        )
         db.session.add(cloud_file)
         db.session.commit()
 
-        with open(save_path, 'wb') as f:  # 保存 文件 & 加密后的文件
+        with open(save_path, "wb") as f:  # 保存 文件 & 加密后的文件
             f.write(encrypted_content_bytes)
 
         return cloud_file
-    
 
     @staticmethod
-    def delete_uncommit(cloud_file_id_:int):
-        """删除数据库中的文件元数据 & 删除本地的加密后的文件
-        """
+    def delete_uncommit(cloud_file_id_: int):
+        """删除数据库中的文件元数据 & 删除本地的加密后的文件"""
         cloud_file = db.session.get(CloudFile, cloud_file_id_)
         if cloud_file is None:
             return False
-        
-        save_path = os.path.join(app.config['UPLOAD_FOLDER'], cloud_file.file_save_name)
+
+        save_path = os.path.join(app.config["UPLOAD_FOLDER"], cloud_file.file_save_name)
         if os.path.exists(save_path):
             os.remove(save_path)
-        
+
         db.session.delete(cloud_file)
         db.session.commit()
 
         return True
-
 
     def decrypt(self) -> bytes:
         """对文件内容进行解密"""
@@ -100,9 +115,9 @@ class CloudFile(db.Model):
 
         return self.decrypted_content_bytes
 
-
     def get_size_str(self) -> str:
         import math
+
         if self.file_size == 0:
             return "0B"
         size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
